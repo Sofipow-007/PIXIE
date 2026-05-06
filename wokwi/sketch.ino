fcc0c93ac6d3f536bd76378fd0de0020c6b47c1a
L6 - PC 12 <power.sofiaet36@gmail.com>
2026-05-06 18:15:35 -0300

    Nueva rama con nuevos archivos que van a servir para mostrar la estructura del hardware con la extensión de wokwi

---
+++ wokwi/sketch.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDR 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDR)) {
+    Serial.println("Error al iniciar la OLED");
+    while (true);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 20);
+
+  display.println("Hello ESP32!");
+  display.display();
+}
+
+void loop() {
+}
