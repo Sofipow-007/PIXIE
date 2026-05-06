5f927158a9213dc38680b01c326fbf162d1c86f4
L6 - PC 12 <power.sofiaet36@gmail.com>
2026-05-06 18:28:17 -0300

    Cara animada agregada al OLED (Solo funciona en Wokwi)

--- wokwi/sketch.ino
+++ wokwi/sketch.ino
@@ -4,26 +4,87 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define SCREEN_ADDR 0x3C
 
+// OLED I2C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// ---------- CARAS ----------
+
+// Cara normal
+void caraNormal() {
+  display.clearDisplay();
+
+  // Ojos
+  display.fillCircle(40, 24, 8, WHITE);
+  display.fillCircle(88, 24, 8, WHITE);
+
+  // Boca feliz
+  display.drawLine(50, 45, 78, 45, WHITE);
+  display.drawPixel(49, 44, WHITE);
+  display.drawPixel(79, 44, WHITE);
+
+  display.display();
+}
+
+// Cara parpadeando
+void caraParpadeo() {
+  display.clearDisplay();
+
+  // Ojos cerrados
+  display.drawLine(32, 24, 48, 24, WHITE);
+  display.drawLine(80, 24, 96, 24, WHITE);
+
+  // Boca feliz
+  display.drawLine(50, 45, 78, 45, WHITE);
+
+  display.display();
+}
+
+// Cara sorprendida
+void caraSorprendida() {
+  display.clearDisplay();
+
+  // Ojos grandes
+  display.drawCircle(40, 24, 8, WHITE);
+  display.drawCircle(88, 24, 8, WHITE);
+
+  // Boca abierta
+  display.drawCircle(64, 46, 6, WHITE);
+
+  display.display();
+}
+
+// ---------- SETUP ----------
 
 void setup() {
   Serial.begin(115200);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDR)) {
-    Serial.println("Error al iniciar la OLED");
+  // Inicializar OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("No se encontró la OLED");
     while (true);
   }
 
   display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 20);
-
-  display.println("Hello ESP32!");
   display.display();
 }
 
+// ---------- LOOP ----------
+
 void loop() {
+
+  // Cara normal
+  caraNormal();
+  delay(1500);
+
+  // Parpadeo rápido
+  caraParpadeo();
+  delay(150);
+
+  caraNormal();
+  delay(1000);
+
+  // Cara sorprendida
+  caraSorprendida();
+  delay(1200);
 }
